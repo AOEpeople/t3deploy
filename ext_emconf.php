efe28e5bb692a2e51b4f5041bbd663e87b2117b3
Michael Klapper <development@morphodo.com>
2012-09-06 12:31:36 +0200

    Add initial version of tx_t3deploy

---
+++ ext_emconf.php
@@ -0,0 +1,32 @@
+<?php
+global $EM_CONF,$_EXTKEY;
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 't3deploy TYPO3 dispatcher',
+	'description' => '',
+	'category' => 'be',
+	'author' => 'AOE media GmbH',
+	'author_email' => 'dev@aoemedia.de',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'createDirs' => '',
+	'modify_tables' => '',
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+			'typo3'=>'4.2.0-',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => '',
+);
