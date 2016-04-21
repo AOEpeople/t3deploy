03c0d6823919b37a47f130565d535895adc1b990
Felix Semmler <felix.semmler@aoe.com>
2016-04-21 14:58:07 +0200

    [TASK] Increase test coverage

--- tests/class.tx_t3deploy_tests_databaseController_testcase.php
+++ tests/class.tx_t3deploy_tests_databaseController_testcase.php
@@ -67,7 +67,7 @@ class tx_t3deploy_tests_databaseController_testcase extends tx_phpunit_database_
 	 */
 	public function doesUpdateStructureActionReportChanges() {
 		$arguments = array(
-			'--verbose' => '',
+			'--verbose' => ''
 		);
 
 		$result = $this->controller->updateStructureAction($arguments);
@@ -82,6 +82,7 @@ class tx_t3deploy_tests_databaseController_testcase extends tx_phpunit_database_
 		$this->assertContains('ALTER TABLE pages ADD tx_testextension_field_test', $result);
 		$this->assertContains('ALTER TABLE pages CHANGE alias alias varchar(255)', $result);
 		$this->assertContains('CREATE TABLE tx_testextension_test', $result);
+		$this->assertNotContains('DROP TABLE tx_testextension', $result);
 	}
 
 	/**
@@ -92,8 +93,7 @@ class tx_t3deploy_tests_databaseController_testcase extends tx_phpunit_database_
 	 */
 	public function doesUpdateStructureActionExecuteChanges() {
 		$arguments = array(
-			'--execute' => '',
-			'--verbose' => '',
+			'--execute' => ''
 		);
 
 		$result = $this->controller->updateStructureAction($arguments);
@@ -101,11 +101,117 @@ class tx_t3deploy_tests_databaseController_testcase extends tx_phpunit_database_
 		// Assert that tables have been created:
 		$tables = $GLOBALS['TYPO3_DB']->admin_get_tables();
 		$pagesFields = $GLOBALS['TYPO3_DB']->admin_get_fields('pages');
+		$this->assertTrue(isset($tables['tx_testextension']));
 		$this->assertTrue(isset($tables['tx_testextension_test']));
 		$this->assertTrue(isset($pagesFields['tx_testextension_field_test']));
 		$this->assertEquals('varchar(255)', strtolower($pagesFields['alias']['Type']));
 
 		// Assert that nothing is reported we just want to execute:
+		$this->assertEmpty($result);
+	}
+
+	/**
+	 * Test whether the updateStructure action just reports remove old database definitions.
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function doesUpdateStructureActionReportRemovals() {
+		$arguments = array(
+			'--remove' => '',
+			'--verbose' => ''
+		);
+
+		$result = $this->controller->updateStructureAction($arguments);
+
+		// Assert that nothing has been removed, this is just for reporting:
+		$tables = $GLOBALS['TYPO3_DB']->admin_get_tables();
+		$this->assertTrue(isset($tables['tx_testextension']));
+		$pagesFields = $GLOBALS['TYPO3_DB']->admin_get_fields('pages');
+		$this->assertTrue(isset($pagesFields['tx_testextension_field']));
+
+		// Assert that removals are reported:
+		$this->assertContains('DROP TABLE tx_testextension', $result);
+		$this->assertContains('ALTER TABLE pages DROP tx_testextension_field', $result);
+	}
+
+	/**
+	 * Test whether the updateStructure action remove old database definitions.
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function doesUpdateStructureActionExecuteRemovals() {
+		$arguments = array(
+			'--remove' => '',
+			'--execute' => ''
+		);
+
+		$result = $this->controller->updateStructureAction($arguments);
+
+		// Assert that tables and columns have been removed:
+		$tables = $GLOBALS['TYPO3_DB']->admin_get_tables();
+		$this->assertFalse(isset($tables['tx_testextension']));
+		$pagesFields = $GLOBALS['TYPO3_DB']->admin_get_fields('pages');
+		$this->assertFalse(isset($pagesFields['tx_testextension_field']));
+
+		// Assert that nothing is reported we just want to execute:
+		$this->assertEmpty($result);
+	}
+
+	/**
+	 * Test whether the updateStructure action remove old database definitions.
+	 *
+	 * test
+	 * @return void
+	 */
+	public function doesUpdateStructureActionReportDropKeys() {
+		$arguments = array(
+			'--drop-keys' => '',
+			'--verbose' => ''
+		);
+
+		$result = $this->controller->updateStructureAction($arguments);
+
+		// Assert that nothing has been removed, this is just for reporting:
+		$tables = $GLOBALS['TYPO3_DB']->admin_get_tables();
+		$this->assertTrue(isset($tables['tx_testextension']));
+		$pagesFields = $GLOBALS['TYPO3_DB']->admin_get_fields('pages');
+		$this->assertTrue(isset($pagesFields['tx_testextension_field']));
+
+		// Assert that removals are reported:
+		$this->assertContains('DROP TABLE tx_testextension', $result);
+		$this->assertContains('ALTER TABLE pages DROP tx_testextension_field', $result);
+	}
+
+	/**
+	 * Test whether the updateStructure action dump changes to file.
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function doesUpdateStructureActionDumpChangesToFile() {
+		$testDumpFile = PATH_tx_t3deploy . 'tests/test_dumpfile.sql';
+		if (file_exists($testDumpFile)) {
+			unlink($testDumpFile);
+		}
+		$this->assertFileNotExists($testDumpFile);
+
+		$arguments = array(
+			'--verbose' => '',
+			'--dump-file' => array($testDumpFile)
+		);
+
+		$result = $this->controller->updateStructureAction($arguments);
+
+		$this->assertFileExists($testDumpFile);
+		$testDumpFileContent = file_get_contents($testDumpFile);
+		// Assert that changes are dumped:
+		$this->assertContains('ALTER TABLE pages ADD tx_testextension_field_test', $testDumpFileContent);
+		$this->assertContains('ALTER TABLE pages CHANGE alias alias varchar(255)', $testDumpFileContent);
+		$this->assertContains('CREATE TABLE tx_testextension_test', $testDumpFileContent);
+
+		// Assert that dump result is reported:
 		$this->assertNotEmpty($result);
 	}
 
