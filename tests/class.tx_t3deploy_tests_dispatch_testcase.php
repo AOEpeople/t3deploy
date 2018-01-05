56c1f6aaf49fc97bd6d366367c26df5b0b6bbd21
Chetan Thapliyal <chetan.thapliyal@aoe.com>
2018-01-05 10:44:32 +0100

    [TASK] TYPO3 Coding Guideline
    - Use whitespaces rather than tabs.
    - Put opening curly bracket in class and method defition on new line.

--- tests/class.tx_t3deploy_tests_dispatch_testcase.php
+++ tests/class.tx_t3deploy_tests_dispatch_testcase.php
@@ -16,67 +16,71 @@ require_once PATH_tx_t3deploy . 'classes/class.tx_t3deploy_dispatch.php';
  * @package t3deploy
  * @author Oliver Hader <oliver.hader@aoe.com>
  */
-class tx_t3deploy_tests_dispatch_testcase extends Tx_Phpunit_TestCase {
-	const ClassPrefix = 'tx_t3deploy_';
-	const ClassSuffix = 'Controller';
+class tx_t3deploy_tests_dispatch_testcase extends Tx_Phpunit_TestCase
+{
+    const ClassPrefix = 'tx_t3deploy_';
+    const ClassSuffix = 'Controller';
 
-	/**
-	 * @var string
-	 */
-	private $testClassName;
+    /**
+     * @var string
+     */
+    private $testClassName;
 
-	/**
-	 * @var tx_t3deploy_dispatch
-	 */
-	private $dispatch;
+    /**
+     * @var tx_t3deploy_dispatch
+     */
+    private $dispatch;
 
-	/**
-	 * Sets up the test cases.
-	 *
-	 * @return void
-	 */
-	public function setUp() {
-		$_SERVER['argv'] = array();
+    /**
+     * Sets up the test cases.
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        $_SERVER['argv'] = [];
 
-		$this->testClassName = uniqid('testClassName');
-		eval('class ' . self::ClassPrefix . $this->testClassName . self::ClassSuffix . ' {}');
+        $this->testClassName = uniqid('testClassName');
+        eval('class ' . self::ClassPrefix . $this->testClassName . self::ClassSuffix . ' {}');
 
-		$this->dispatch = new tx_t3deploy_dispatch();
-	}
+        $this->dispatch = new tx_t3deploy_dispatch();
+    }
 
-	/**
-	 * Cleans up the test cases.
-	 *
-	 * @return void
-	 */
-	public function tearDown() {
-		unset($this->dispatch);
-	}
+    /**
+     * Cleans up the test cases.
+     *
+     * @return void
+     */
+    public function tearDown()
+    {
+        unset($this->dispatch);
+    }
 
-	/**
-	 * Tests whether a controller is correctly dispatched
-	 *
-	 * @test
-	 */
-	public function isControllerActionCorrectlyDispatched() {
-		$cliArguments = array(
-			'_DEFAULT' => array(
-				't3deploy',
-				$this->testClassName,
-				'test'
-			)
-		);
+    /**
+     * Tests whether a controller is correctly dispatched
+     *
+     * @test
+     */
+    public function isControllerActionCorrectlyDispatched()
+    {
+        $cliArguments = [
+            '_DEFAULT' => [
+                't3deploy',
+                $this->testClassName,
+                'test'
+            ]
+        ];
 
-		$testMock = $this->getMock($this->testClassName, array('testAction'));
-		$testMock->expects($this->once())->method('testAction')->willReturn($this->testClassName);
+        $testMock = $this->getMock($this->testClassName, ['testAction']);
+        $testMock->expects($this->once())->method('testAction')->willReturn($this->testClassName);
 
-		$this->dispatch->setCliArguments($cliArguments);
-		$this->dispatch->setClassInstance(
-			self::ClassPrefix . $this->testClassName . self::ClassSuffix,
-			$testMock
-		);
-		$result = $this->dispatch->dispatch();
+        $this->dispatch->setCliArguments($cliArguments);
+        $this->dispatch->setClassInstance(
+            self::ClassPrefix . $this->testClassName . self::ClassSuffix,
+            $testMock
+        );
+        $result = $this->dispatch->dispatch();
 
-		$this->assertEquals($this->testClassName, $result);
-	}
+        $this->assertEquals($this->testClassName, $result);
+    }
 }
