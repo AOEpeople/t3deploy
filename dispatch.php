89eec0cdae03bbef4371405a436a61b0705436e1
Chetan Thapliyal <chetan.thapliyal@aoe.com>
2018-01-08 11:22:31 +0100

    [CLEANUP] Update copyright and replace tabs with spaces

--- dispatch.php
+++ dispatch.php
@@ -2,14 +2,14 @@
 /***************************************************************
 *  Copyright notice
 *
-*  (c) 2016 AOE GmbH <dev@aoe.com>
+*  (c) 2018 AOE GmbH <dev@aoe.com>
 *  All rights reserved
 *
 *  This copyright notice MUST APPEAR in all copies of the script!
 ***************************************************************/
 
 if (!defined ('TYPO3_cliMode')) {
-	die('Access denied: CLI only.');
+    die('Access denied: CLI only.');
 }
 
 require_once PATH_tx_t3deploy . 'Classes/class.tx_t3deploy_dispatch.php';
