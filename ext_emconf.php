1a420c944903fe5b684f3f811c4012fdb6f73b36
Chetan Thapliyal <chetan.thapliyal@aoe.com>
2018-01-09 12:56:30 +0100

    [TASK] Update version to 1.2.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,10 +10,10 @@ $EM_CONF[$_EXTKEY] = array(
     'clearCacheOnLoad' => 0,
     'author' => 'AOE GmbH',
     'author_email' => 'dev@aoe.com',
-    'version' => '1.1.1',
+    'version' => '1.2.0',
     'constraints' => array(
         'depends' => array(
-            'typo3'=>'6.2.0-7.6.99',
+            'typo3'=>'7.6.0-8.7.99',
         ),
         'conflicts' => array(),
         'suggests' => array(),
